c2086ad9112d78da0a5bb72e29e708648b55e8b1
Felix Schindler <felix.schindler@wwu.de>
2017-08-09 14:45:13 +0200

    expose all to jupyter notebook

--- start_notebook_server.py
+++ start_notebook_server.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NOTEBOOK_PATH=$PWD/notebooks
+export NOTEBOOK_PATH=$PWD
 export NOTEBOOK_PORT=${EXPOSED_PORT:-18881}
 
 jupyter-notebook --ip 0.0.0.0 --no-browser --notebook-dir=$NOTEBOOK_PATH --port=$NOTEBOOK_PORT
